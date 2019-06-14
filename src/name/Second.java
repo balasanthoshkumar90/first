9bc69119290eb2130dd38cc688f0404bbcaf020e
balasanthoshkumar90 <balasanthoshr@gmail.com>
2019-06-14 11:55:28 +0530

    first push

---
+++ src/name/Second.java
@@ -0,0 +1,17 @@
+package name;
+
+public class Second {
+	public void jesima(){
+	 	System.out.println("jesima");
+	}
+	private void santhosh() {
+	System.out.println("68745324");
+
+	}
+public static void main(String[] args) {
+	
+	Second e=new Second();
+	e.jesima();
+	
+}
+}
